c4280e173d7ec5f5bf4ea4eed9faf4a13b2f9daf
Ruby M <ruby.mamistvalove@gmail.com>
2024-11-09 16:20:49 +0200

    Form skeleton

--- src/app/add-new-user/add-new-user.component.ts
+++ src/app/add-new-user/add-new-user.component.ts
@@ -6,10 +6,28 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-add-new-user',
   standalone: true,
-  imports: [],
-  template: ` <section></section> `,
+  imports: [ReactiveFormsModule],
+  template: `
+    <article>
+      <form [formGroup]="applyForm" (submit)="submitApplication()">
+        <label for="name">Name</label>
+        <input id="name" type="text" formControlName="name" />
+        <label for="birthdate">Birthdate</label>
+        <input id="birthdate" type="text" formControlName="birthdate" />
+        <button type="submit" class="primary">Save</button>
+      </form>
+    </article>
+  `,
   styleUrl: './add-new-user.component.css',
 })
 export class AddNewUserComponent {
   usersService: UsersService = inject(UsersService);
+  applyForm = new FormGroup({
+    name: new FormControl(''),
+    birthdate: new FormControl(''),
+  });
+
+  submitApplication() {
+    console.log('here');
+  }
 }
